d0afddf813354ee21eb2e292f8fa694335c35543
Leonel Caroc <leonelcaroc25@gmail.com>
2023-11-27 13:42:06 +0800

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import HardReload from "vite-plugin-hard-reload";
+import FullReload from "vite-plugin-hard-reload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), HardReload()],
+  plugins: [react(), FullReload()],
   server: {
     open: "http://localhost:3000",
     port: 3000,
