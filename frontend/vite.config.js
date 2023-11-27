e90f3b5a1a8adfdf241cd2cf4ab71ee1561104da
Leonel Caroc <leonelcaroc25@gmail.com>
2023-11-27 13:43:02 +0800

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import FullReload from "vite-plugin-hard-reload";
+import FullReload from "vite-plugin-full-reload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
