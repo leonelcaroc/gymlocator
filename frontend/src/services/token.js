ce29c7c20ff59dde1028e8a158dd13f9de395f8a
Leonel Caroc <leonelcaroc25@gmail.com>
2024-01-08 10:55:07 +0800

    update Tokenservice

--- frontend/src/services/token.js
+++ frontend/src/services/token.js
@@ -2,6 +2,7 @@
 // const cookies = new Cookies();
 
 class TokenService {
+  // Admin Token Service
   getAdminLocal() {
     return localStorage.getItem("adminInfo");
   }
@@ -12,6 +13,45 @@ class TokenService {
 
   removeAdminLocal() {
     return localStorage.removeItem("adminInfo");
+  }
+
+  // Gym Owner Token Service
+  getOwnerLocal() {
+    return localStorage.getItem("ownerInfo");
+  }
+
+  setOwnerLocal(token) {
+    return localStorage.setItem("ownerInfo", token);
+  }
+
+  removeOwnerLocal() {
+    return localStorage.removeItem("ownerInfo");
+  }
+
+  // User Token Service
+  getUserLocal() {
+    return localStorage.getItem("userInfo");
+  }
+
+  setUserLocal(token) {
+    return localStorage.setItem("userInfo", token);
+  }
+
+  removeUserLocal() {
+    return localStorage.removeItem("userInfo");
+  }
+
+  // Trainer Token Service
+  getTrainerLocal() {
+    return localStorage.getItem("trainerInfo");
+  }
+
+  setTrainerLocal(token) {
+    return localStorage.setItem("trainerInfo", token);
+  }
+
+  removeTrainerLocal() {
+    return localStorage.removeItem("trainerInfo");
   }
 }
 
