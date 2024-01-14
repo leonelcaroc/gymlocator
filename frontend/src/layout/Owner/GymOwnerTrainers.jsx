75e91d0b7be721bd81670cdd684f9dfaf5e6efe3
Leonel Caroc <leonelcaroc25@gmail.com>
2024-01-14 22:25:55 +0800

    update Trainers

--- frontend/src/layout/Owner/GymOwnerTrainers.jsx
+++ frontend/src/layout/Owner/GymOwnerTrainers.jsx
@@ -1,37 +1,121 @@
-import React from "react";
+import { useState } from "react";
 import {
-  Text,
+  Checkbox,
+  CheckboxGroup,
+  CloseButton,
   Box,
   Flex,
   Button,
   Input,
+  InputGroup,
+  InputRightElement,
+  ListItem,
   TableContainer,
   Table,
+  Text,
+  Textarea,
   Thead,
   Tr,
   Th,
   Td,
   Tbody,
   Modal,
-  Select,
   ModalOverlay,
   ModalContent,
   ModalHeader,
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Checkbox,
-  CheckboxGroup,
+  Select,
   Stack,
+  OrderedList,
+  UnorderedList,
   useDisclosure,
 } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 
 const GymOwnerTrainers = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const newUniqueId = uuidv4();
+  const {
+    isOpen: isAddNewTrainerOpen,
+    onOpen: openAddNewTrainer,
+    onClose: closeAddNewTrainer,
+  } = useDisclosure();
+  const [newCertificate, setNewCertificate] = useState({
+    certificateName: "",
+    key: "",
+  });
+  const [newSpecialty, setNewSpecialty] = useState({
+    specialtyName: "",
+    key: "",
+  });
+  const [newTrainer, setNewTrainer] = useState({
+    firstname: "",
+    middlename: "",
+    lastname: "",
+    email: "",
+    contact: "",
+    address: "",
+    dateOfBirth: null,
+    gender: "",
+    certifications: [],
+    specialties: [],
+    yearsOfExperience: 0,
+    biography: "",
+  });
+
+  const handleDeleteCertification = (index) => {
+    setNewTrainer((prevTrainer) => {
+      const updatedCertifications = [...prevTrainer.certifications];
+      updatedCertifications.splice(index, 1);
+
+      return {
+        ...prevTrainer,
+        certifications: updatedCertifications,
+      };
+    });
+  };
+
+  const handleSubmitNewTrainer = () => {
+    console.log(newTrainer);
+    setNewTrainer({
+      firstname: "",
+      middlename: "",
+      lastname: "",
+      email: "",
+      contact: "",
+      address: "",
+      dateOfBirth: null,
+      gender: "",
+      certifications: [],
+      specialties: [],
+      yearsOfExperience: 0,
+      biography: "",
+    });
+    closeAddNewTrainer();
+  };
+
+  const handleCloseNewTrainer = () => {
+    setNewTrainer({
+      firstname: "",
+      middlename: "",
+      lastname: "",
+      email: "",
+      contact: "",
+      address: "",
+      dateOfBirth: null,
+      gender: "",
+      certifications: [],
+      specialties: [],
+      yearsOfExperience: 0,
+      biography: "",
+    });
+    closeAddNewTrainer();
+  };
 
   return (
     <Box padding="2rem">
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isAddNewTrainerOpen} onClose={handleCloseNewTrainer}>
         <ModalOverlay />
         <ModalContent paddingInline="2rem" maxWidth="35rem">
           <ModalHeader>Add Trainer</ModalHeader>
@@ -40,74 +124,247 @@ const GymOwnerTrainers = () => {
             <Stack spacing="1rem">
               <Box>
                 <Text fontWeight="bold">First Name</Text>
-                <Input type="text" placeholder="First Name" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      firstname: e.target.value,
+                    })
+                  }
+                  type="text"
+                  placeholder="First Name"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">Middle Name</Text>
-                <Input type="text" placeholder="Middle Name" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      middlename: e.target.value,
+                    })
+                  }
+                  type="text"
+                  placeholder="Middle Name"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">Last Name</Text>
-                <Input type="text" placeholder="Last Name" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      lastname: e.target.value,
+                    })
+                  }
+                  type="text"
+                  placeholder="Last Name"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">Email</Text>
-                <Input type="email" placeholder="Email" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      email: e.target.value,
+                    })
+                  }
+                  type="email"
+                  placeholder="Email"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">Contact</Text>
-                <Input type="text" placeholder="Contact" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      contact: e.target.value,
+                    })
+                  }
+                  type="text"
+                  placeholder="Contact"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">Address</Text>
-                <Input type="text" placeholder="Address" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      address: e.target.value,
+                    })
+                  }
+                  type="text"
+                  placeholder="Address"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">BirthDate</Text>
-                <Input type="date" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      dateOfBirth: e.target.value,
+                    })
+                  }
+                  type="date"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">Gender</Text>
-                <Select placeholder="Select Gender">
-                  <option value="option1">Male</option>
-                  <option value="option2">Female</option>
+                <Select
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      gender: e.target.value,
+                    })
+                  }
+                  placeholder="Select Gender"
+                >
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
                 </Select>
               </Box>
               <Stack spacing="0.8rem">
                 <Text fontWeight="bold">Certifications</Text>
-                <CheckboxGroup colorScheme="green">
-                  <Stack spacing={[1, 5]} direction={["column", "row"]}>
-                    <Checkbox value="naruto">Personal Trainer</Checkbox>
-                    <Checkbox value="sasuke">Yoga Instructor</Checkbox>
-                    <Checkbox value="kakashi">Pilates Instructor</Checkbox>
-                  </Stack>
-                </CheckboxGroup>
+                <InputGroup>
+                  <Input
+                    value={newCertificate.certificateName}
+                    onChange={(e) =>
+                      setNewCertificate({
+                        certificateName: e.target.value,
+                        key: newUniqueId,
+                      })
+                    }
+                    type="text"
+                  />
+                  <InputRightElement>
+                    <Button
+                      height="70%"
+                      bgColor="brand.100"
+                      marginRight="1rem"
+                      onClick={() => {
+                        if (newCertificate.certificateName !== "") {
+                          setNewTrainer((prevTrainer) => ({
+                            ...prevTrainer,
+                            certifications: [
+                              ...prevTrainer.certifications,
+                              newCertificate,
+                            ],
+                          }));
+                          setNewCertificate({
+                            certificateName: "",
+                            key: "",
+                          });
+                        }
+                      }}
+                    >
+                      Add
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+
+                <UnorderedList>
+                  {newTrainer.certifications?.map((item, index) => (
+                    <Flex key={item.key} justify="space-between" align="center">
+                      <ListItem>{item.certificateName}</ListItem>
+                      <CloseButton
+                        onClick={() => handleDeleteCertification(index)}
+                      />
+                    </Flex>
+                  ))}
+                </UnorderedList>
               </Stack>
               <Stack spacing="0.8rem">
                 <Text fontWeight="bold">Specialties</Text>
-                <CheckboxGroup colorScheme="green">
-                  <Stack spacing={[1, 5]} direction={["column", "row"]}>
-                    <Checkbox value="naruto">Strength Training</Checkbox>
-                    <Checkbox value="sasuke">Cardio</Checkbox>
-                    <Checkbox value="kakashi">Nutrition</Checkbox>
-                  </Stack>
-                </CheckboxGroup>
+                <InputGroup>
+                  <Input
+                    value={newSpecialty.specialtyName}
+                    onChange={(e) =>
+                      setNewSpecialty({
+                        specialtyName: e.target.value,
+                        key: newUniqueId,
+                      })
+                    }
+                    type="text"
+                  />
+                  <InputRightElement>
+                    <Button
+                      height="70%"
+                      bgColor="brand.100"
+                      marginRight="1rem"
+                      onClick={() => {
+                        if (newSpecialty.specialtyName !== "") {
+                          setNewTrainer((prevTrainer) => ({
+                            ...prevTrainer,
+                            specialties: [
+                              ...prevTrainer.specialties,
+                              newSpecialty,
+                            ],
+                          }));
+                          setNewSpecialty({
+                            specialtyName: "",
+                            key: "",
+                          });
+                        }
+                      }}
+                    >
+                      Add
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+
+                <UnorderedList>
+                  {newTrainer.specialties?.map((item) => {
+                    return (
+                      <ListItem key={item.key}>{item.specialtyName}</ListItem>
+                    );
+                  })}
+                </UnorderedList>
               </Stack>
               <Box>
                 <Text fontWeight="bold">Years of Experience</Text>
-                <Input type="number" />
+                <Input
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      yearsOfExperience: parseInt(e.target.value, 10),
+                    })
+                  }
+                  onKeyDown={(e) => {
+                    const isNumeric = /^[0-9\b]+$/.test(e.key);
+                    if (!isNumeric) {
+                      e.preventDefault();
+                    }
+                  }}
+                  type="number"
+                />
               </Box>
               <Box>
                 <Text fontWeight="bold">Biography</Text>
-                <Input type="text" />
+                <Textarea
+                  onChange={(e) =>
+                    setNewTrainer({
+                      ...newTrainer,
+                      biography: e.target.value,
+                    })
+                  }
+                  type="text"
+                />
               </Box>
             </Stack>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button colorScheme="blue" mr={3} onClick={handleCloseNewTrainer}>
               Close
             </Button>
-            <Button bgColor="brand.100" color="neutral.100">
+            <Button
+              bgColor="brand.100"
+              color="neutral.100"
+              onClick={handleSubmitNewTrainer}
+            >
               Add Trainer
             </Button>
           </ModalFooter>
@@ -116,7 +373,11 @@ const GymOwnerTrainers = () => {
       <Text color="brand.200" fontSize="2rem" marginBottom="2rem">
         Trainers
       </Text>
-      <Button bgColor="brand.100" color="neutral.100" onClick={onOpen}>
+      <Button
+        bgColor="brand.100"
+        color="neutral.100"
+        onClick={openAddNewTrainer}
+      >
         Add Trainer
       </Button>
       <TableContainer marginTop="2rem">
